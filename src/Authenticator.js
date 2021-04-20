f10bdc14d389928a7161d1afc3cac410d8cf7f77
paul <cruskit@gmail.com>
2021-04-20 21:10:51 +1000

    Clean out tutorial code. Basic renames.

---
+++ src/Authenticator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Authenticator extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <p>Auth URL: </p>
+                <p>ClientId: </p>
+                <p>Client Secret: </p>
+            </div>
+        )
+    }
+
+}
+
+export default Authenticator;
